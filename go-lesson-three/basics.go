20cb33071543662c52bfaf089c89e6bd2c2dd4d7
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-01-10 08:54:47 +0300

    Lesson_03 Pointers_01

--- go-lesson-three/basics.go
+++ go-lesson-three/basics.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Lesson three. Start with pointers")
+	fmt.Println("Lesson_3:")
+	runPointer()
 }
+
+//func runLessons() {
+//	runLesson(runPointer)
+//	runLesson(runMap)
+//	runLesson(runStructure)
+//	runLesson(runPublicPrivate)
+//}
+//
+////Pointer
+//
+//func printArrayAndPointer(array [3]string, pointer *[3]string) {
+//
+//}
+//
+//func changeNames(array [3]string, pointer *[3]string) {
+//
+//}
+
+func runPointer() {
+	var mySalary int64 = 1250
+	var yourSalary int64 = mySalary
+	fmt.Printf("Моя зарплата: %d, твоя зарплата: %d \n", mySalary, yourSalary)
+
+	mySalary += 200
+	fmt.Printf("Моя зарплата: %d, твоя зарплата: %d \n", mySalary, yourSalary)
+
+	yourSalary += 100
+	fmt.Printf("Моя зарплата: %d, твоя зарплата: %d \n", mySalary, yourSalary)
+
+}
+
+//Map
