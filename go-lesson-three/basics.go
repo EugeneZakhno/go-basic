3779596ff5649e99dc4b91fc28499e36dc286089
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-01-10 15:28:38 +0300

    Lesson_03 Pointers_03

--- go-lesson-three/basics.go
+++ go-lesson-three/basics.go
@@ -7,22 +7,25 @@ func main() {
 	runPointer()
 }
 
-//func runLessons() {
-//	runLesson(runPointer)
-//	runLesson(runMap)
-//	runLesson(runStructure)
-//	runLesson(runPublicPrivate)
-//}
+//	func runLessons() {
+//		runLesson(runPointer)
+//		runLesson(runMap)
+//		runLesson(runStructure)
+//		runLesson(runPublicPrivate)
+//	}
 //
-////Pointer
-//
-//func printArrayAndPointer(array [3]string, pointer *[3]string) {
-//
-//}
-//
-//func changeNames(array [3]string, pointer *[3]string) {
-//
-//}
+
+// Pointer
+func printArrayAndPointer(array [3]string, pointer *[3]string) {
+	fmt.Println("Массив =", array, "; Указатель на array", pointer, "; Значения указателя pointer =", *pointer)
+}
+
+func changeNames(array [3]string, pointer *[3]string) {
+	array[0] = "Carrie_White"
+	pointer[2] = "Donnie_Darko"
+
+	printArrayAndPointer(array, pointer)
+}
 
 func runPointer() {
 	var mySalary int64 = 1250
@@ -48,9 +51,46 @@ func runPointer() {
 	//fmt.Println(&mySavings == yourSavings)
 
 	mySavings += mySalary
+	fmt.Printf("Мои сбережения: %d, твои сбережения: %d \n", mySavings, yourSavings)
 
-	fmt.Println("Мои сбережения: %d, твои сбережения: %d \n", mySavings, yourSavings)
+	var defaultInt64 int64
+	var defaultInt64Pointer *int64
+	fmt.Println("defaultInt64 =", defaultInt64, "; defaultInt64Pointer =", defaultInt64Pointer)
+	fmt.Println("defaultInt64Pointer is nil?", defaultInt64Pointer == nil)
+	// fmt.Println(*defaultInt64Pointer)
 
+	var namesArray [3]string = [3]string{"Dana", "Bob", "Alice"}
+	var namesPointer *[3]string = &namesArray
+	printArrayAndPointer(namesArray, namesPointer)
+
+	changeNames(namesArray, namesPointer)
+	printArrayAndPointer(namesArray, namesPointer)
+
+	names := []string{"Dana", "Alice", "Bob"}
+	// var pointer **[] string = &&name
+	// var pointer **[] string = &(&names)
+	pointer1 := &names
+	pointer2 := &pointer1
+	fmt.Println("Массив:", names)
+	fmt.Println("pointer1", pointer1, "; *pointer1", *pointer1)
+	fmt.Println("pointer2", pointer2, "; *pointer2", *pointer2, "; **pointer2 =", **pointer2)
+
+	var matreshka1 string = "MАТРЁШКА"
+	var matreshka2 *string = &matreshka1
+	var matreshka3 **string = &matreshka2
+	fmt.Println("Основная матрёшка:", matreshka1, "; Указатель на неё:", matreshka2, "; Указатель на указатель:", matreshka3)
+	fmt.Println("Oсновная матрёшка:", matreshka1, "; значение указателя на неё:", *matreshka2, "; Значение указателя на указатель:", **matreshka3)
+
+	**matreshka3 = "НОВАЯ_МАТРЁШКА"
+	fmt.Println("Oсновная матрёшка:", matreshka1, "; указатель на неё:", matreshka2, "; Указатель на указатель:", matreshka3)
+	fmt.Println("Основная матрёшка:", matreshka1, "; Значение указателя на неё:", *matreshka2, "; Значение указателя на указатель:", **matreshka3)
+
+	var matreshka4 ***string = &matreshka3
+	var matreshka5 ****string = &matreshka4
+	fmt.Println(matreshka4, ***matreshka4, matreshka5, ****matreshka5)
 }
 
-//Map
+// Map
+func printBoolStringMap(mapBoolString map[bool]string) {
+
+}
