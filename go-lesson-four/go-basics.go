354a1523d5467416969aba6ce22f293be44066f0
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-01-14 16:34:54 +0300

    Lesson_04 int values

--- go-lesson-four/go-basics.go
+++ go-lesson-four/go-basics.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Lesson_04")
+	demonstrateMaxMinForTypes()
+}
+
+const ()
+
+type Author struct {
+}
+
+func runLessons() {
+	//runLesson (runMath)
+	//runLesson (runRandom)
+	//runLesson (runSlicesSort)
+	//runLesson (runDefer)
+	//runLesson (runErrors)
+	//runLesson (runReadWriteToFile)
+	//runLesson (runReadFromConsole)
+}
+
+// MAth
+func demonstrateMaxMinForTypes() {
+	var maxInt8Value int8 = math.MaxInt8
+	var maxInt16Value int16 = math.MaxInt16
+	var maxInt32Value int32 = math.MaxInt32
+	var maxInt64Value int64 = math.MaxInt64
+	fmt.Println("Max values for int ->  ", maxInt8Value, maxInt16Value, maxInt32Value, maxInt64Value)
+
+	var Int8Value int8
+	var Int16Value int16
+	var Int32Value int32
+	var Int64Value int64
+	fmt.Println("Values for int by default ->  ", Int8Value, Int16Value, Int32Value, Int64Value)
+
+	var minInt8Value int8 = math.MinInt8
+	var minInt16Value int16 = math.MinInt16
+	var minInt32Value int32 = math.MinInt32
+	var minInt64Value int64 = math.MinInt64
+	fmt.Println("Min values for int ->  ", minInt8Value, minInt16Value, minInt32Value, minInt64Value)
+
 }
