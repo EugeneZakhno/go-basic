2e02c04008de8dc44586ac01651a30ac8d2a870a
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-01-16 09:38:23 +0300

    Lesson_05 Time_01

--- go-lesson-five/basics.go
+++ go-lesson-five/basics.go
@@ -5,3 +5,29 @@ import "fmt"
 func main() {
 	fmt.Println("Lesson_05")
 }
+func demonstrateDuration() {}
+
+func demonstrateTimeDifference() {
+}
+func runTime() {
+	// import "time"
+	//demonstrateBasicDateCreation()
+	//demonstrateTimeDateCreation()
+	//demonstrateTimeUnixCreation()
+	//demonstrateFormatToString()
+	//demonstrateParse()
+	//	demonstrateComparison()
+	demonstrateDuration()
+	demonstrateTimeDifference()
+}
+
+// Интерфейсы
+func printSomething(index int, i interface{}) {
+}
+func runInterfaces() {
+}
+
+// Утверждение типов
+//func parseStringToInt8(value string) (int, error) {
+//	return
+//}
