ed93b25324184da5895a8082a6e57e040d781f59
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-01-16 16:32:45 +0300

    Lesson_05 Time_04

--- go-lesson-five/basics.go
+++ go-lesson-five/basics.go
@@ -18,8 +18,8 @@ func runTime() {
 	demonstrateBasicDateCreation()
 	demonstrateTimeDateCreation()
 	demonstrateTimeUnixCreation()
-	//demonstrateFormatToString()
-	//demonstrateParse()
+	demonstrateFormatToString()
+	demonstrateParse()
 	//	demonstrateComparison()
 	//demonstrateDuration()
 	demonstrateTimeDifference()
@@ -85,5 +85,51 @@ func demonstrateTimeUnixCreation() {
 	fmt.Println("Точка отсчёта", myLocation, time.Unix(0, 0).In(myLocation))
 }
 
-func demonstrateFormatToString() {}
-func demonstrateParse()          {}
+func demonstrateFormatToString() {
+	// Стандарт: 2 января 2006 года 15:04:05 MST-7
+	var angelinaJolieBirthDate time.Time
+	var locations [100]string
+	usaLocation, _ := time.LoadLocation(locations[0])
+	angelinaJolieBirthDate = time.Date(1975, time.June, 4, 9, 8, 5, 1, usaLocation) // 4 июня 1975 09:08:05.01
+	fmt.Println(angelinaJolieBirthDate)
+	var angelinaJolieStrBirthDate string = angelinaJolieBirthDate.Format("02.01.2006 15:04:05")
+	fmt.Println(angelinaJolieStrBirthDate)
+	fmt.Println(angelinaJolieBirthDate.Format("2.1.06 3:4:5"))
+	fmt.Println(angelinaJolieBirthDate.Format("2006-01-02"))
+	fmt.Println(angelinaJolieBirthDate.Format("02.01.2006"))
+	fmt.Println(angelinaJolieBirthDate.Format("15:04:05"))
+	fmt.Println(angelinaJolieBirthDate.Format("Минуты: 04"))
+	fmt.Println(angelinaJolieBirthDate.Format("Дата рождения: 02.01.2006 15 часов 04 минут 05 секунд"))
+	fmt.Println(angelinaJolieBirthDate.Format("02 Jan 2006"))
+	fmt.Println(angelinaJolieBirthDate.Format("02 Feb 2006"))
+	fmt.Println(angelinaJolieBirthDate.Format("01/02/2006"))
+	fmt.Println(angelinaJolieBirthDate.Format("01/03/2006")) // month -> hours
+	fmt.Println(angelinaJolieBirthDate.Format("01/07/2006"))
+}
+
+func demonstrateParse() {
+	// Стандарт: 2 января 2006 года 15:04:05 MST-7
+	var audreyHepburnBirthDate time.Time
+	var audreyHepburnBirthDateStr string = "04.05.1929 03:02:01" // 4 Мая 1929 3:02:01
+	audreyHepburnBirthDate, _ = time.Parse("02.01.2006 15:04:05", audreyHepburnBirthDateStr)
+	fmt.Println("День рождения Одри:", audreyHepburnBirthDate)
+	parsedDate, err := time.Parse("День рождения: 02.01", "День рождения: 04.05")
+	fmt.Println(parsedDate)
+	fmt.Println(err)
+	parsedDate, err = time.Parse("День рождения: 02.01", "04.05")
+	fmt.Println(parsedDate)
+	fmt.Println(err)
+	parsedDate, err = time.Parse("15:04:05", " 03:02")
+	fmt.Println(parsedDate)
+	fmt.Println(err)
+	parsedDate, err = time.Parse("15:4", "03:02")
+	fmt.Println(parsedDate)
+	fmt.Println(err)
+	fmt.Println(time.Parse("2006-01-02T15:04:05Z", "1929-05-04T03:02:01Z"))
+	fmt.Println(time.Parse("02.01.2006", "04.05.1929"))
+	fmt.Println(time.Parse("T15:04:05Z", "T03:02:01Z"))
+	fmt.Println(time.Parse("15:04:05", "03:02:01"))
+	fmt.Println(time.Parse("15:04", "03:02"))
+	fmt.Println(time.Parse("2006-01-02", "1929-05-04"))
+	fmt.Println(time.Parse("01/02/2006", "04/05/1929"))
+}
