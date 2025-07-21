52454e514b16c6927cdd25a3bf23caee9cf40722
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-07-21 12:56:52 +0300

    Lesson_03: revising of pointers

--- go-lesson-three/pointers.go
+++ go-lesson-three/pointers.go
@@ -8,11 +8,16 @@ func main() {
 	println("5.Значение  ", a)
 	squareVal(a)
 	println(a)
+	squareAdd(&a)
 }
 func changeValueThroughPointer(p *int) {
 	println("3. Из другой функции ", p)
 	sqr := *p * *p
 	*p = sqr
+}
+
+func squareAdd(p *int) { // There is a copy of v
+	println(p)
 }
 
 func squareVal(v int) { // There is a copy of v
